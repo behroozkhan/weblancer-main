59fb37f39891b116f3b28aacdd9a41aa585d23de
unknown <behrooz.arabzade@gmail.com>
2020-08-13 15:44:54 +0430

    testing

--- utils/loadDotEnv.js
+++ utils/loadDotEnv.js
@@ -1,9 +1,9 @@
 import dotenv from 'dotenv';
 import path from 'path';
 const __dirname = path.resolve();
-console.log("configing", __dirname + "/.env", process.env);
+console.log("configing", __dirname + "/.env", process.env.DATABASE_USER);
 dotenv.config({ path: __dirname + "/.env" });
 
-console.log("configed", __dirname + "/.env", process.env);
+console.log("configed", __dirname + "/.env", process.env.DATABASE_USER);
 
 export default dotenv;
