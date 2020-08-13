df25cd38b4fc8d300730380ffb5bfcf9245ec5eb
unknown <behrooz.arabzade@gmail.com>
2020-08-13 16:18:51 +0430

    testing

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use('/server', server);
 app.use('/middle', middle);
  
 console.log("index.js", 8)
-sequelize.sync({force: true, logging: true}).then(() => {
+sequelize.sync({logging: true}).then(() => {
     console.log("index.js", 9)
     app.listen(process.env.PORT, () => {
         console.log(`Weblancer main express server now listening on port ${process.env.PORT}!`);
