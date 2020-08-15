f6bc580329d46cdb25293799a31e6ffb8a1dee58
unknown <behrooz.arabzade@gmail.com>
2020-08-15 13:29:30 +0430

    preparation

--- models/index.js
+++ models/index.js
@@ -1,11 +1,9 @@
-'use strict';
+import Sequelize from 'sequelize';
+import models from './models.js';
+import configJson from '../config/config.json';
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = configJson[env];
 const db = {};
 
 let sequelize;
@@ -15,15 +13,21 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
+// fs
+//   .readdirSync(__dirname)
+//   .filter(file => {
+//     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js') &&
+//     (file !== 'models.js');
+//   })
+//   .forEach(file => {
+//     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+//   });
+
+for (let model in models) {
+    if (models.hasOwnProperty(model)) {
+      db[model.name] = models[model];
+    }
+}
 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
