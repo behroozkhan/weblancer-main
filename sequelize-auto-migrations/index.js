c1b667061a4d95a684af1460d7acbe46e8cc6849
unknown <behrooz.arabzade@gmail.com>
2020-08-15 13:56:07 +0430

    preparation

---
+++ sequelize-auto-migrations/index.js
@@ -0,0 +1,4 @@
+
+import migrate from "./lib/migrate";
+
+module.exports = migrate;
---
+++ sequelize-auto-migrations/lib/pathconfig.js
@@ -0,0 +1,35 @@
+import path from 'path';
+import fs from 'fs';
+
+module.exports = async function(options){
+    let sequelizercConfigs = [],
+        sequelizercPath = path.join(process.env.PWD, '.sequelizerc');
+
+    if (fs.existsSync(sequelizercPath)){
+        sequelizercConfigs = await import(sequelizercPath);
+    }
+    
+    if(!process.env.PWD){
+        process.env.PWD = process.cwd()
+    }
+    
+    let migrationsDir = path.join(process.env.PWD, 'migrations'),
+        modelsDir = path.join(process.env.PWD, 'models');
+        
+    if (options['migrations-path']) {
+        migrationsDir = path.join(process.env.PWD, options['migrations-path']);
+    } else if (sequelizercConfigs['migrations-path']) {
+        migrationsDir = sequelizercConfigs['migrations-path'];
+    }
+    
+    if (options['models-path']) {
+        modelsDir = path.join(process.env.PWD, options['models-path']);
+    } else if (sequelizercConfigs['models-path']) {
+        modelsDir = sequelizercConfigs['models-path'];
+    }
+    
+    return {
+        migrationsDir: migrationsDir,
+        modelsDir: modelsDir
+    }
+}
