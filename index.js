0c2c31591347c9e37e75f6ca192d53473ea2991e
behroozkhan <52956849+behroozkhan@users.noreply.github.com>
2020-08-15 16:57:20 +0430

    Update index.js

--- index.js
+++ index.js
@@ -4,20 +4,6 @@ let { unlessRoute } = require('./utils/utils.js');
 let express = require('express');
 let cors = require('cors');
 
-<<<<<<< HEAD
-import payment from './routes/payment.js';
-import editor from './routes/editor.js';
-import hosting from './routes/hosting.js';
-import publisher from './routes/publisher.js';
-import transaction from './routes/transaction.js';
-import plan from './routes/plan.js';
-import server from './routes/server.js';
-import middle from './routes/middle.js';
-import Response from './utils/response.js';
-import cors from 'cors';
-
-const appBaseRoute = '/whitelabel';
-=======
 let payment = require('./routes/payment.js');
 let editor = require('./routes/editor.js');
 let hosting = require('./routes/hosting.js');
@@ -27,14 +13,11 @@ let plan = require('./routes/plan.js');
 let server = require('./routes/server.js');
 let middle = require('./routes/middle.js');
 const Response = require('./utils/response.js');
->>>>>>> a833008c613929c62b59e620b708864b9f21bb00
+const appBaseRoute = '/whitelabel';
 
 let app = express();
 app.use(cors());
-<<<<<<< HEAD
 app.options('*', cors());
-=======
->>>>>>> a833008c613929c62b59e620b708864b9f21bb00
 
 app.use(express.json());
 app.use(unlessRoute([
@@ -62,11 +45,7 @@ app.get(appBaseRoute + '/test', function (req, res) {
     );
 })
  
-<<<<<<< HEAD
-sequelize.sync({logging: false}).then(() => {
-=======
 sequelize.sync({logging: true}).then(() => {
->>>>>>> a833008c613929c62b59e620b708864b9f21bb00
     app.listen(process.env.PORT, () => {
         console.log(`Weblancer main express server now listening on port ${process.env.PORT}!`);
     });
