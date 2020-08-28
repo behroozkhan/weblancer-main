9ddde6f7b60cf01c09fdba4e6ce9bc638c987d6f
unknown <behrooz.arabzade@gmail.com>
2020-08-28 19:51:59 +0430

    publisher auth

--- acl/authorization.js
+++ acl/authorization.js
@@ -4,6 +4,7 @@ let { models } = require('../model-manager/models.js');
 module.exports.authorizeToken = function authorizeToken(req, res, next) {
     const publisherId = req.headers['publisher_id'];
     const publisherPassword = req.headers['publisher_password'];
+    console.log("authorizeToken headers", req.headers);
 
     if (publisherId) {
         console.log("authorizeToken publisherId", publisherId);
@@ -15,7 +16,6 @@ module.exports.authorizeToken = function authorizeToken(req, res, next) {
                 res.status(401).json(
                     new Response(false, {}, "id and password are not match").json()
                 );
-                return;
             } else {
                 req.user = publisher.toJSON();
                 next();
@@ -31,10 +31,17 @@ module.exports.authorizeToken = function authorizeToken(req, res, next) {
         console.log("authorizeToken authHeader", authHeader);
         const token = authHeader && authHeader.split(' ')[1];
     
-        if (token == null) res.sendStatus(401);
+        if (token == null) {
+            res.sendStatus(401);
+            return;
+        }
     
         jwt.verify(token, process.env.JWT_ACCESS_TOKEN_SECRET, (err, user) => {
-            if (err) res.sendStatus(403);
+            if (err) {
+                res.sendStatus(403);
+                return;
+            }
+
             req.user = user;
             next();
         });
